7cabfa0bc6d1a0884b35f4902aded42483d04272
Fatema Tuj Zohra <ftzohratit@gmail.com>
2018-02-12 20:38:26 -0500

    WebAutomation1004 maven project created in Eclipse

---
+++ src/main/java/com/seleniumwebdriver/WebAutomation1004/App.java
@@ -0,0 +1,13 @@
+package com.seleniumwebdriver.WebAutomation1004;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
